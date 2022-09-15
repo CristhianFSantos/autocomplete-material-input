cf82d406a9e8158fc36e6a43af417144b40f30fd
Cristhian Santos <cristhianfelipe0@gmail.com>
2022-09-15 19:48:42 -0300

    ✨ feat: create material input with autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+
+export const NAMES = ['Ana','Antonio','Cristiane','Eduardo','Elisa',]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'select-complete';
+export class AppComponent implements OnInit {
+  formName: FormGroup;
+  
+  
+  filterNames: Observable<string[]>
+
+
+  ngOnInit(): void {
+    this.formName = new FormGroup({
+      inputControl : new FormControl(null)
+    })
+
+
+    this.filterNames = this.formName.controls['inputControl'].valueChanges.pipe(
+      startWith(''),
+      map(value => this.filter(value || '' )))
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = this.normalizeValue(value);
+    return NAMES.filter(street => this.normalizeValue(street).includes(filterValue));
+  }
+
+  private normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +19,8 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
